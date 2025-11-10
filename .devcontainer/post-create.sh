3da1ba8f641585b575faeacb56892c341c196006
Suoi-Nguon Pham <aetheriofficial@gmail.com>
2025-11-10 02:00:10 -0800

    Refactor post-create.sh for clarity and consistency
    Updated comments and variable names for clarity. Changed 'environment.yml' to 'environment.yaml' for consistency.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,14 @@
 # .devcontainer/post-create.sh
 set -euxo pipefail
 
+# ===== AzCopy Installation (Ubuntu 24.04) =====
+# Useful for streaming web-hosted datasets directly into Azure Blob Storage Containers.
+#
+# Allows for greater project flexibility, by removing the need to always carry the 
+# physical hard drive containing the ~2+ TB datasets, and having code directly interface 
+# with the cloud-hosted data anywhere in the world on any device.
 
-# --- AzCopy (Ubuntu 24.04) --- (https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azcopy-install-linux-package?tabs=apt)
+# Installation Docs: https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azcopy-install-linux-package?tabs=apt
 curl -sSL -O https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
@@ -10,26 +16,32 @@ sudo apt-get update
 sudo apt-get install -y azcopy
 
 
-# --- micromamba init ---
-export MAMBA_ROOT_PREFIX="$HOME/micromamba"
+# ===== Micromamba Init =====
+# Many population synthesis and post-processing code is written inside a conda-forge Python environment.
+#
+# Conda-forge is typically setup via the Miniforge3 installer; 
+# Micromamba is chosen here instead due to its compatibility with container environments.
+
+# Initialize micromamba in the current shell
+export MAMBA_ROOT="$HOME/micromamba"
 eval "$(micromamba shell hook -s bash)"
 
-# Future shell persistance
+# Intialize micromamba in future shell recurrences
 {
-    echo 'export MAMBA_ROOT_PREFIX="$HOME/micromamba"'
+    echo 'export MAMBA_ROOT="$HOME/micromamba"'
     echo 'eval "$(micromamba shell hook -s bash)"'
 } >> "$HOME/.bashrc"
 
-# Strict channel enforcement (conda-forge)
+# Enforce conda-forge channel
 micromamba config set channel_priority strict
 
-# Create/update env from environment.yml
-ENV_NAME="sspc"
-if [ -f environment.yml ]; then
-    micromamba create -y -n "$ENV_NAME" -f environment.yml || micromamba install -y -n "$ENV_NAME" -f environment.yml
+# Create/update sspc env from environment.yaml
+ENV="sspc"
+if [ -f environment.yaml ]; then
+    micromamba create -y -n "$ENV" -f environment.yml || micromamba install -y -n "$ENV" -f environment.yml
 else
-    micromamba create -y -n "$ENV_NAME" python=3.13 
+    micromamba create -y -n "$ENV" python=3.13 
 fi
 
-# Auto-activate micromamba in shell
-echo "micromamba activate $ENV_NAME" >> "$HOME/.bashrc"
+# Auto-activate env in the current shell
+echo "micromamba activate $ENV" >> "$HOME/.bashrc"
